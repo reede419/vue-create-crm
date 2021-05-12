dc8f5303621bf4a1c1daf87f8c9638508dbbd000
Reede4 <s.klepach@smsclub.mobi>
2021-05-12 16:14:47 +0300

    add vuevalidate (validate form)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+import Vuelidate from 'vuelidate'
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -9,6 +11,8 @@ import'materialize-css/dist/js/materialize.min.js'
 Vue.config.productionTip = false;
 
 Vue.filter('date', dateFilter)
+Vue.use(Vuelidate)
+
 
 new Vue({
   router,
