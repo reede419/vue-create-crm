9d07e05ddb21e1ec600366c564c4f5402e82e9dc
Reede4 <s.klepach@smsclub.mobi>
2021-05-13 10:48:22 +0300

    create message utils

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import router from "./router";
 import store from "./store";
 import dateFilter from './filters/date.filter'
 import'materialize-css/dist/js/materialize.min.js'
+import messagePlugin from './utils/message.plugin'
 
 Vue.config.productionTip = false;
 
 Vue.filter('date', dateFilter)
 Vue.use(Vuelidate)
+Vue.use(messagePlugin)
 
 
 new Vue({
