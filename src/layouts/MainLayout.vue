b7f20ac540eed512eda09daef20a63b0c8a752b9
Reede4 <s.klepach@smsclub.mobi>
2021-05-19 15:57:06 +0300

    protect routes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,6 +24,7 @@
 
 import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
+import messages from '../utils/messages'
 
 export default {
   name: 'main-layout',
@@ -40,6 +41,16 @@ export default {
   },
   components: {
     Navbar, Sidebar
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Something went wrong')
+    }
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import firebase from "firebase/app"
 
 Vue.use(VueRouter);
 
@@ -19,43 +20,43 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Home.vue'),
   },
   {
     path: "/categories",
     name: "categories",
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Categories.vue'),
   },
   {
     path: "/detail_record",
     name: "detail_record",
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/DetailRecord.vue'),
   },
   {
     path: "/history",
     name: "history",
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/History.vue'),
   },
   {
     path: "/planning",
     name: "planning",
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Planning.vue'),
   },
   {
     path: "/profile",
     name: "profile",
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Profile.vue'),
   },
   {
     path: "/record",
     name: "record",
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Record.vue'),
   }
 ];
@@ -65,5 +66,16 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(auth => auth.meta.auth)
+
+  if( requireAuth && !currentUser ) {
+    next('/login')
+  } else {
+    next()
+  }
+})
 
 export default router;
